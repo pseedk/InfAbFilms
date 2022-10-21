68d124adf115bdc478110a599aee43c61c16b152
pseedk <pseedk@ya.ru>
2022-10-21 15:46:25 +0300

    Day-5

--- app/src/main/java/com/pseedk/infabfilms/navigation/SetupNavHost.kt
+++ app/src/main/java/com/pseedk/infabfilms/navigation/SetupNavHost.kt
@@ -32,7 +32,6 @@ fun SetupNavHost(navController: NavHostController, viewModel: MainViewModel) {
         }
         composable(route = Screens.Details.route + "/{Id}") { backStackEntry ->
             DetailsScreen(
-                navController = navController,
                 viewModel = viewModel,
                 itemId = backStackEntry.arguments?.getString("Id") ?: "1"
             )
--- app/src/main/java/com/pseedk/infabfilms/utils/Constants.kt
+++ app/src/main/java/com/pseedk/infabfilms/utils/Constants.kt
@@ -1,5 +1,11 @@
 package com.pseedk.infabfilms.utils
 
+import android.widget.TextView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.text.HtmlCompat
+
 class Constants {
 
     object Screens {
@@ -7,4 +13,13 @@ class Constants {
         const val MAIN_SCREEN = "main_screen"
         const val DETAILS_SCREEN = "detail_screen"
     }
+}
+
+@Composable
+fun HtmlText(html: String, modifier: Modifier = Modifier) {
+    AndroidView(
+        modifier = modifier,
+        factory = { context -> TextView(context) },
+        update = { it.text = HtmlCompat.fromHtml(html, HtmlCompat.FROM_HTML_MODE_COMPACT)}
+    )
 }
